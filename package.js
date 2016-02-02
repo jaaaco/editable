4d0decba86dece4170f0cdccd97ae2dd2f279cb3
Jakub Wietrzyk <jakub.wietrzyk@wfirma.pl>
2016-02-02 17:22:47 +0100

    added jquery dep

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
-  name: 'jaaaco:editable',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'Form inputs that automagically save data to collection',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jaaaco/editable.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'jaaaco:editable',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'Form inputs that automagically save data to collection',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/jaaaco/editable.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.addFiles('editable.js',['client']);
+Package.onUse(function (api) {
+    api.versionsFrom('1.2.1');
+    api.use('jquery');
+    api.addFiles('editable.js', ['client']);
 });
